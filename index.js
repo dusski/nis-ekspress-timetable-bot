cd7e36d6b34d7900b2e469c85448c547708b304a
dusski <dusski@gmail.com>
2018-01-28 11:46:09 +0100

    Added scraping

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 require("dotenv").load();
 
-const BootBot = require("bootbot");
+const BootBot = require("bootbot"),
+	request = require("request"),
+	cheerio = require("cheerio");
 
 const bot = new BootBot({
 	accessToken: process.env.FB_ACCESS_TOKEN,
@@ -11,8 +13,41 @@ const bot = new BootBot({
 });
 
 bot.start(process.env.PORT);
+let json = { title: "", release: "", rating: "" };
 
+// web scraping part
+const url = "http://www.imdb.com/title/tt1229340/";
+request(url, (err, res, html) => {
+	if (err) throw err;
+
+	let $ = cheerio.load(html);
+
+	$(".header").filter(function() {
+		let data = $(this);
+		json.title = data
+			.children()
+			.first()
+			.text();
+
+		json.release = data
+			.children()
+			.last()
+			.children()
+			.text();
+	});
+
+	$(".star-box-giga-star").filter(function() {
+		var data = $(this);
+		json.rating = data.text();
+	});
+});
+
+// response part
 bot.on("message", (payload, chat) => {
 	const text = payload.message.text;
-	chat.say(`Echo: ${text}`);
+	if (text === "/get-movie") {
+		chat.say(JSON.stringify(json));
+	} else {
+		chat.say(`Echo: ${text}`);
+	}
 });
