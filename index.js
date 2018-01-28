693750ac4d9f4074adf6f3712893a6e53e66cf88
dusski <dusski@gmail.com>
2018-01-28 10:05:19 +0100

    Changed to dot notation for BootBot data

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ require("dotenv").load();
 const BootBot = require("bootbot");
 
 const bot = new BootBot({
-	accessToken: process.env["FB_ACCESS_TOKEN"],
-	verifyToken: process.env["FB_VERIFY_TOKEN"],
-	appSecret: process.env["FB_APP_SECRET"]
+	accessToken: process.env.FB_ACCESS_TOKEN,
+	verifyToken: process.env.FB_VERIFY_TOKEN,
+	appSecret: process.env.FB_APP_SECRET
 });
 
 bot.start(process.env.PORT);
