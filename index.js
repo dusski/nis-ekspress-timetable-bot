13d202f5b44b96131ae23b2a6456ca409e1376d3
dusski <dusski@gmail.com>
2018-01-28 10:02:47 +0100

    BootBot added

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+require("dotenv").load();
+
+const BootBot = require("bootbot");
+
+const bot = new BootBot({
+	accessToken: process.env["FB_ACCESS_TOKEN"],
+	verifyToken: process.env["FB_VERIFY_TOKEN"],
+	appSecret: process.env["FB_APP_SECRET"]
+});
+
+bot.start(process.env.PORT);
+
+bot.on("message", (payload, chat) => {
+	const text = payload.message.text;
+	chat.say(`Echo: ${text}`);
+});
