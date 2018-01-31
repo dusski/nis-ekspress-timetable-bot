b23e8fd6e28071fd99286c82f807eb2b8600c718
dusski <dusski@gmail.com>
2018-01-31 23:32:18 +0100

    Added variable case input

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ bot.hear("/help", (payload, chat) => {
     - N > K (Niš > Kočane R.)`);
 });
 
-bot.hear("D > N", (payload, chat) => {
+bot.hear(["D > N", "D>N", "d > n", "d>n", "dn", "DN"], (payload, chat) => {
 	const url =
 		"http://195.178.51.120/WebReservations/Home/SearchForJourneys?inNext=1&timeFlagNow=true&tb_calendar=28.01.2018&tb_FromTime=00%3A00&FromPointName=DOLJEVAC&ToPointName=NI%C5%A0&FromPointNameId=3088&ToPointNameId=2710&filterPassengerId=1&RoundtripProcessing=false&ValidityUnlimited=True&Timetable=True";
 
@@ -49,56 +49,65 @@ bot.hear("D > N", (payload, chat) => {
 	});
 });
 
-bot.hear("N > D", async (payload, chat) => {
-	const url =
-		"http://195.178.51.120/WebReservations/Home/SearchForJourneys?inNext=1&timeFlagNow=true&tb_calendar=28.01.2018&tb_FromTime=00%3A00&FromPointName=NI%C5%A0&ToPointName=DOLJEVAC&FromPointNameId=2710&ToPointNameId=3088&filterPassengerId=1&RoundtripProcessing=false&ValidityUnlimited=True&Timetable=True";
+bot.hear(
+	["N > D", "N>D", "n > d", "n>d", "nd", "ND"],
+	async (payload, chat) => {
+		const url =
+			"http://195.178.51.120/WebReservations/Home/SearchForJourneys?inNext=1&timeFlagNow=true&tb_calendar=28.01.2018&tb_FromTime=00%3A00&FromPointName=NI%C5%A0&ToPointName=DOLJEVAC&FromPointNameId=2710&ToPointNameId=3088&filterPassengerId=1&RoundtripProcessing=false&ValidityUnlimited=True&Timetable=True";
 
-	request(url, (err, res, html) => {
-		if (err) throw err;
+		request(url, (err, res, html) => {
+			if (err) throw err;
 
-		let $ = cheerio.load(html);
+			let $ = cheerio.load(html);
 
-		const output = $(".listing-border > tbody")
-			.children()
-			.map((i, el) => (i < 3 ? el : null))
-			.text();
+			const output = $(".listing-border > tbody")
+				.children()
+				.map((i, el) => (i < 3 ? el : null))
+				.text();
 
-		chat.say(output);
-	});
-});
+			chat.say(output);
+		});
+	}
+);
 
-bot.hear("K > N", async (payload, chat) => {
-	const url =
-		"http://195.178.51.120/WebReservations/Home/SearchForJourneys?inNext=1&timeFlagNow=true&tb_calendar=28.01.2018&tb_FromTime=00%3A00&FromPointName=KO%C4%8CANE+R.&ToPointName=NI%C5%A0&FromPointNameId=5443&ToPointNameId=2710&filterPassengerId=1&RoundtripProcessing=false&ValidityUnlimited=True&Timetable=True";
+bot.hear(
+	["K > N", "K>N", "k > n", "k>n", "kn", "KN"],
+	async (payload, chat) => {
+		const url =
+			"http://195.178.51.120/WebReservations/Home/SearchForJourneys?inNext=1&timeFlagNow=true&tb_calendar=28.01.2018&tb_FromTime=00%3A00&FromPointName=KO%C4%8CANE+R.&ToPointName=NI%C5%A0&FromPointNameId=5443&ToPointNameId=2710&filterPassengerId=1&RoundtripProcessing=false&ValidityUnlimited=True&Timetable=True";
 
-	request(url, (err, res, html) => {
-		if (err) throw err;
+		request(url, (err, res, html) => {
+			if (err) throw err;
 
-		let $ = cheerio.load(html);
+			let $ = cheerio.load(html);
 
-		const output = $(".listing-border > tbody")
-			.children()
-			.map((i, el) => (i < 3 ? el : null))
-			.text();
+			const output = $(".listing-border > tbody")
+				.children()
+				.map((i, el) => (i < 3 ? el : null))
+				.text();
 
-		chat.say(output);
-	});
-});
+			chat.say(output);
+		});
+	}
+);
 
-bot.hear("N > K", async (payload, chat) => {
-	const url =
-		"http://195.178.51.120/WebReservations/Home/SearchForJourneys?inNext=1&timeFlagNow=true&tb_calendar=28.01.2018&tb_FromTime=00%3A00&FromPointName=NI%C5%A0&ToPointName=KO%C4%8CANE+R.&FromPointNameId=2710&ToPointNameId=5443&filterPassengerId=1&RoundtripProcessing=false&ValidityUnlimited=True&Timetable=True";
+bot.hear(
+	["N > K", "N>K", "n > k", "n>k", "nk", "NK"],
+	async (payload, chat) => {
+		const url =
+			"http://195.178.51.120/WebReservations/Home/SearchForJourneys?inNext=1&timeFlagNow=true&tb_calendar=28.01.2018&tb_FromTime=00%3A00&FromPointName=NI%C5%A0&ToPointName=KO%C4%8CANE+R.&FromPointNameId=2710&ToPointNameId=5443&filterPassengerId=1&RoundtripProcessing=false&ValidityUnlimited=True&Timetable=True";
 
-	request(url, (err, res, html) => {
-		if (err) throw err;
+		request(url, (err, res, html) => {
+			if (err) throw err;
 
-		let $ = cheerio.load(html);
+			let $ = cheerio.load(html);
 
-		const output = $(".listing-border > tbody")
-			.children()
-			.map((i, el) => (i < 3 ? el : null))
-			.text();
+			const output = $(".listing-border > tbody")
+				.children()
+				.map((i, el) => (i < 3 ? el : null))
+				.text();
 
-		chat.say(output);
-	});
-});
+			chat.say(output);
+		});
+	}
+);
