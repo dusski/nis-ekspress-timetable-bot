f165063a348a8bb8f54add570fb40375b94dc085
dusski <dusski@gmail.com>
2018-08-25 14:08:20 +0200

    Removed me object

--- index.js
+++ index.js
@@ -259,15 +259,13 @@ const jgp = async (userInput) => {
 
 	if (!userInput) return "Wrong input, try again!";
 
-	let me = {
-		buslineCode: {},
-		dayToday: moment().day() == 0 ? 2 : (moment().day() == 6 ? 1 : 0),
-		busTimetable: "",
-		numberOfHoursToShow: 5,
-		currentTime: moment().hours(),
-		result: [],
-		counter: 0
-	}
+	const buslineCode = {},
+		dayToday = moment().day() == 0 ? 2 : (moment().day() == 6 ? 1 : 0),
+		busTimetable = "",
+		numberOfHoursToShow = 5,
+		currentTime = moment().hours(),
+		result = [],
+		counter = 0;
 
 	let $ = cheerio.load(await axios.get(process.env.BASE_CB_URL));
 
@@ -279,36 +277,36 @@ const jgp = async (userInput) => {
 			code = $(item).first().next().find("button").attr("data-target");
 		}
 		if (busline && code)
-			me.buslineCode[busline] = code;
+			buslineCode[busline] = code;
 	});
 
-	me.busTimetable = $($(me.buslineCode[jgpData[userInput]])
+	busTimetable = $($(buslineCode[jgpData[userInput]])
 		.find(".nav-tabs > li")[dayToday])
 		.find("a").attr("href");
 
-	$(`${me.busTimetable} > table > tbody > tr`).map((index, item) => {
+	$(`${busTimetable} > table > tbody > tr`).map((index, item) => {
 		if (index < 1) return "";
 
 		let hour = $(item).children().first().text();
 		let minutes = $($(item).children()[1]).text();
 
-		if (me.currentTime < 4) {
+		if (currentTime < 4) {
 			if (index < 5) {
-				me.result.push(`${hour} | ${minutes}`);
+				result.push(`${hour} | ${minutes}`);
 			}
 			if (index > 19) {
-				me.result.splice(me.counter, 0, `${hour} | ${minutes}`);
-				me.counter++;
+				result.splice(counter, 0, `${hour} | ${minutes}`);
+				counter++;
 			}
 		} else {
 			if ((index + 3) >= currentTime && (index + 3) < (currentTime + numberOfHoursToShow)) {
-				me.result.push(`${hour} | ${minutes}`);
+				result.push(`${hour} | ${minutes}`);
 			}
 		}
 
 	});
 
-	return me.result.join("\n");
+	return result.join("\n");
 
 }
 
