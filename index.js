a5f9d0c33c26e69f6673c51e1934e7ccb8968bf9
dusski <dusski@gmail.com>
2019-01-26 22:41:51 +0100

    Revert "Adjusted hour addition"
    This reverts commit 78ad65dfc1a8834b0706962f2937f5258d964b2f.

--- index.js
+++ index.js
@@ -222,7 +222,7 @@ const jgp = async (userInput) => {
 		dayToday = moment().day() == 0 ? 2 : (moment().day() == 6 ? 1 : 0),
 		busTimetable = "",
 		numberOfHoursToShow = 4,
-		currentTime = moment().add(2, "hours").hours(),
+		currentTime = moment().hours() + 2,
 		result = [],
 		counter = 0;
 
